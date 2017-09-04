f61037e524281e97d01db6314f53297647ff0df0
Shinkei <ing.jorgeivanramirezh@gmail.com>
2017-09-04 17:31:18 -0500

    change modal variables

--- src/app/components/time-modal/time-modal.component.ts
+++ src/app/components/time-modal/time-modal.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
 
 export interface ConfirmModel {
-  title:string;
-  message:string;
+  startDate:Date;
+  endDate:Date;
+  weekDays:string[];
 }
 
 @Component({
@@ -14,14 +15,15 @@ export interface ConfirmModel {
 })
 export class TimeModalComponent extends DialogComponent<ConfirmModel, boolean> implements ConfirmModel {
 
-  title: string;
-  message: string;
+  startDate: Date;
+  endDate: Date;
+  weekDays: string[];
+
   constructor(dialogService: DialogService) {
     super(dialogService);
   }
   confirm() {
-    // we set dialog result as true on click on confirm button, 
-    // then we can get dialog result from caller code 
+
     this.result = true;
     this.close();
   }
