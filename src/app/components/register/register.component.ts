78ca0c1e03dec46c52dd6d48aa89d66484780ee6
Shinkei <ing.jorgeivanramirezh@gmail.com>
2017-09-04 19:36:34 -0500

    add last fixes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,22 +10,25 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  email:string;
+  password:string;
   fullname:string;
   identification:string;
   typeVehicle:string;
   plate:string;
   description:string;
   listVehicle:any [];
+  response: any;
 
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private parkingService:ParkingmeService) {
     this.listVehicle = [];
    }
 
   add(){
 
     let vehicle = {
-      plate:this.plate,
+      plateNumber:this.plate,
       typeVehicle:this.typeVehicle,
       description:this.description
     }
@@ -47,7 +50,25 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    this.router.navigateByUrl('/home');
+    
+    let user = {
+      email: this.email,
+      password:this.password,
+      name: this.fullname,
+      cc: this.identification,
+      typeVehicle: this.typeVehicle,
+      plate: this.plate,
+      description: this.description,
+      vehicles: this.listVehicle
+    };
+
+console.log(user);
+    this.parkingService.registerUser(user).subscribe((response) => {
+      this.response = response;
+      this.router.navigateByUrl('/home');
+    }, resFileError => {
+    console.log(JSON.parse(resFileError['_body']));
+    });;
   }
 
 }
